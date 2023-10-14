088ee0af5cbcbc27c8d1418bdb09f91e73d36824
Niklas Dewes <niklas.dewes@t-online.de>
2023-10-14 17:24:20 +0200

    added next time to activate in message

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
+import datetime
 
 import discord
 from discord import app_commands
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+token = os.environ.get('TOKEN')
+guild_id = os.environ.get('GUILD')
 
 class ADB(discord.Client):
     def __init__(self):
@@ -12,19 +15,22 @@ class ADB(discord.Client):
         self.synced = False
 
     async def on_ready(self):
-        await tree.sync(guild=discord.Object(id=os.environ.get("GUILD")))
+        await tree.sync(guild=discord.Object(id=guild_id))
         self.synced = True
-        print(f"Logged in as {self.user.name}")
+        print(f'Logged in as {self.user.name}')
 
 bot = ADB()
 tree = app_commands.CommandTree(bot)
 
 @tree.command(
-    name="activate",
-    description="Activate your Badge",
-    guild=discord.Object(id=os.environ.get("GUILD")),
+    name='activate',
+    description='Activate your Badge',
+    guild=discord.Object(id=guild_id),
 )
 async def self(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Your Badge are successfully reloaded.")
+    await interaction.response.send_message('Your Badge are successfully **activated**.')
+    today = datetime.datetime.today()
+    next_activate = (today + datetime.timedelta(days=30)).strftime('%d.%m.%Y %H:%M')
+    await interaction.channel.send(f'Next activate on **{next_activate}**')
 
-bot.run(os.environ.get("TOKEN"))
+bot.run(token=token)
